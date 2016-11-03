a736d62f962699dae43b027257c5b5e325a17013
Skander Jabouzi <skander.jabouzi@geemedia.com>
2016-11-03 09:17:26 -0400

    add gesture files

---
+++ swiftSwipe/BottomToTop.swift
@@ -0,0 +1,9 @@
+//
+//  BottomToTop.swift
+//  swiftSwipe
+//
+//  Created by dev on 11/3/16.
+//  Copyright © 2016 Skander Jabouzi. All rights reserved.
+//
+
+import Foundation
---
+++ swiftSwipe/TopToBottom.swift
@@ -0,0 +1,9 @@
+//
+//  TopToBottom.swift
+//  swiftSwipe
+//
+//  Created by dev on 11/3/16.
+//  Copyright © 2016 Skander Jabouzi. All rights reserved.
+//
+
+import Foundation
