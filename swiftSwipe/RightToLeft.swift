d1ecf9241aa231e3a30f3d4afa1d81d7d42b4f25
Skander Jabouzi <skander.jabouzi@geemedia.com>
2016-11-03 11:30:12 -0400

    add all gestures swift code

--- swiftSwipe/RightToLeft.swift
+++ swiftSwipe/RightToLeft.swift
@@ -6,4 +6,31 @@
 //  Copyright © 2016 Skander Jabouzi. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class RightToLeft : UIStoryboardSegue {
+    
+    override func perform() {
+        let source:UIViewController = self.source;
+        let destination:UIViewController = self.destination;
+        
+        var sourceFrame:CGRect = source.view.frame;
+        sourceFrame.origin.x = -sourceFrame.size.width;
+        
+        var destFrame:CGRect = destination.view.frame;
+        destFrame.origin.x = destination.view.frame.size.width;
+        destination.view.frame = destFrame;
+        
+        destFrame.origin.x = 0;
+        source.view.superview?.addSubview(destination.view);
+        
+        UIView.animate(withDuration: 0.25, animations: {
+            source.view.frame = sourceFrame;
+            destination.view.frame = destFrame;
+        }, completion: {
+            finished in
+            let window: UIWindow = source.view.window!;
+            window.rootViewController = destination;
+        });
+    }
+}
--- swiftSwipe/TopToBottom.swift
+++ swiftSwipe/TopToBottom.swift
@@ -6,4 +6,32 @@
 //  Copyright © 2016 Skander Jabouzi. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class TopToBottom : UIStoryboardSegue {
+    
+    override func perform() {
+        let source:UIViewController = self.source;
+        let destination:UIViewController = self.destination;
+        
+        var sourceFrame:CGRect = source.view.frame;
+        sourceFrame.origin.y = sourceFrame.size.height;
+        
+        var destFrame:CGRect = destination.view.frame;
+        destFrame.origin.y = -destination.view.frame.size.height;
+        destination.view.frame = destFrame;
+        
+        destFrame.origin.y = 0;
+        source.view.superview?.addSubview(destination.view);
+        
+        UIView.animate(withDuration: 0.24, animations: {
+            source.view.frame = sourceFrame;
+            destination.view.frame = destFrame;
+        }, completion: {
+            finished in
+            let window: UIWindow = source.view.window!;
+            window.rootViewController = destination;
+        });
+    }
+
+}
